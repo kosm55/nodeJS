c3db33b1243d91133f72fdd360a3d80ce12fa8d4
Liudmyla <kosmosjatko@gmail.com>
2024-04-18 22:11:26 +0300

    hw2

--- main.js
+++ main.js
@@ -1,57 +1,94 @@
 const fsPromises=require("node:fs/promises")
 const path=require('node:path')
 
+// v1:
+// async function creatingFiles(){
+//     try {
+//         const basePath= path.join(process.cwd(), 'baseFolder')
+//         await fsPromises.mkdir(basePath, { recursive: true})
+//
+//         await fsPromises.mkdir(path.join(basePath, 'one'), { recursive: true})
+//         await fsPromises.writeFile(path.join(process.cwd(), 'baseFolder', 'one', 'one.txt'), 'from 1')
+//
+//         await fsPromises.mkdir(path.join(basePath, 'two'),{ recursive: true})
+//         await fsPromises.writeFile(path.join(basePath, 'two', 'two.txt'), 'from 2')
+//         await fsPromises.mkdir(path.join(basePath, 'three'),{ recursive: true})
+//         await fsPromises.writeFile(path.join(basePath, 'three', 'three.txt'), 'from 3')
+//         await fsPromises.mkdir(path.join(basePath, 'four'),{ recursive: true})
+//         await fsPromises.writeFile(path.join(basePath, 'four', 'four.txt'), 'from 4')
+//         await fsPromises.mkdir(path.join(basePath, 'five'),{ recursive: true})
+//         await fsPromises.writeFile(path.join(basePath, 'five', 'five.txt'), 'from 5')
+//         console.log('-----start')
+//         console.log(path.join(basePath, 'one'))
+//         const oneDir= await fsPromises.stat(path.join(basePath, 'one'))
+//         console.log(`is directory- ${oneDir.isDirectory()}`)
+//         console.log(path.join(basePath, 'one', 'one.txt'))
+//         const oneFile= await fsPromises.stat(path.join(basePath, 'one', 'one.txt'))
+//         console.log(`is file- ${oneFile.isFile()}`)
+//
+//         console.log(path.join(basePath,'two'))
+//         const twoDir=await fsPromises.stat(path.join(basePath, 'two' ))
+//         console.log(`is directory- ${twoDir.isDirectory()}`)
+//         console.log(path.join(basePath, 'two', 'two.txt'))
+//         const twoFile= await fsPromises.stat(path.join(basePath, 'two', 'two.txt'))
+//         console.log(`is file- ${twoFile.isFile()}`)
+//
+//         console.log(path.join(basePath, 'three'))
+//         const threeDir= await fsPromises.stat(path.join(basePath, 'three'))
+//         console.log(`is directory- ${threeDir.isDirectory()}`)
+//         console.log(path.join(basePath, 'three', 'three.txt'))
+//         const threeFile= await fsPromises.stat(path.join(basePath, 'three', 'three.txt'))
+//         console.log(`is file- ${threeFile.isFile()}`)
+//
+//         console.log(path.join(basePath, 'four'))
+//         const fourDir= await fsPromises.stat(path.join(basePath, 'four'))
+//         console.log(`is directory- ${fourDir.isDirectory()}`)
+//         console.log(path.join(basePath, 'four', 'four.txt'))
+//         const fourFile= await fsPromises.stat(path.join(basePath, 'four', 'four.txt'))
+//         console.log(`is file- ${fourFile.isFile()}`)
+//
+//         console.log(path.join(basePath, 'five'))
+//         const fiveDir= await fsPromises.stat(path.join(basePath, 'five'))
+//         console.log(`is directory- ${fiveDir.isDirectory()}`)
+//         console.log(path.join(basePath, 'five', 'five.txt'))
+//         const fiveFile= await fsPromises.stat(path.join(basePath, 'five', 'five.txt'))
+//         console.log(`is file- ${fiveFile.isFile()}`)
+//
+//
+//     }catch (e) {
+//         console.log(e)
+//     }
+// }
+//
+// void creatingFiles()
+
+// or v2:
 async function creatingFiles(){
     try {
-        // await fsPromises.mkdir(path.join(__dirname, 'baseFolder'))
+        const basePath= path.join(process.cwd(), 'baseFolder2')
+        await fsPromises.mkdir(basePath, { recursive: true})
 
-        // await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'one'))
-        // await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'one', 'one.txt'), 'from 1')
+        const folders= ['folder1','folder2','folder3','folder4','folder5']
+        const files= ['file1','file2','file3','file4','file5']
 
-        // await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'two'))
-        // await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'two', 'two.txt'), 'from 2')
-        // await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'three'))
-        // await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'three', 'three.txt'), 'from 3')
-        // await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'four'))
-        // await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'four', 'four.txt'), 'from 4')
-        // await fsPromises.mkdir(path.join(__dirname, 'baseFolder', 'five'))
-        // await fsPromises.writeFile(path.join(__dirname, 'baseFolder', 'five', 'five.txt'), 'from 5')
-        console.log('-----start')
-        console.log(path.join(__dirname,'baseFolder', 'one'))
-        const oneDir= await fsPromises.stat(path.join(__dirname,'baseFolder', 'one'))
-        console.log(`is directory- ${oneDir.isDirectory()}`)
-        console.log(path.join(__dirname, 'baseFolder', 'one', 'one.txt'))
-        const oneFile= await fsPromises.stat(path.join(__dirname, 'baseFolder', 'one', 'one.txt'))
-        console.log(`is file- ${oneFile.isFile()}`)
+        for (const folder of folders) {
+            const folderPath= path.join(basePath, folder)
 
-        console.log(path.join(__dirname, 'baseFolder','two'))
-        const twoDir=await fsPromises.stat(path.join(__dirname,'baseFolder', 'two' ))
-        console.log(`is directory- ${twoDir.isDirectory()}`)
-        console.log(path.join(__dirname, 'baseFolder', 'two', 'two.txt'))
-        const twoFile= await fsPromises.stat(path.join(__dirname, 'baseFolder', 'two', 'two.txt'))
-        console.log(`is file- ${twoFile.isFile()}`)
+            await fsPromises.mkdir(folderPath, {recursive: true})
 
-        console.log(path.join(__dirname, 'baseFolder', 'three'))
-        const threeDir= await fsPromises.stat(path.join(__dirname,'baseFolder', 'three'))
-        console.log(`is directory- ${threeDir.isDirectory()}`)
-        console.log(path.join(__dirname, 'baseFolder', 'three', 'three.txt'))
-        const threeFile= await fsPromises.stat(path.join(__dirname, 'baseFolder', 'three', 'three.txt'))
-        console.log(`is file- ${threeFile.isFile()}`)
+            for (const file of files) {
+                const filePath= path.join(folderPath, `${file}.txt`)
 
-        console.log(path.join(__dirname, 'baseFolder', 'four'))
-        const fourDir= await fsPromises.stat(path.join(__dirname,'baseFolder', 'four'))
-        console.log(`is directory- ${fourDir.isDirectory()}`)
-        console.log(path.join(__dirname, 'baseFolder', 'four', 'four.txt'))
-        const fourFile= await fsPromises.stat(path.join(__dirname, 'baseFolder', 'four', 'four.txt'))
-        console.log(`is file- ${fourFile.isFile()}`)
+                await fsPromises.writeFile(filePath, "hey file")
+            }
+        }
 
-        console.log(path.join(__dirname, 'baseFolder', 'five'))
-        const fiveDir= await fsPromises.stat(path.join(__dirname,'baseFolder', 'five'))
-        console.log(`is directory- ${fiveDir.isDirectory()}`)
-        console.log(path.join(__dirname, 'baseFolder', 'five', 'five.txt'))
-        const fiveFile= await fsPromises.stat(path.join(__dirname, 'baseFolder', 'five', 'five.txt'))
-        console.log(`is file- ${fiveFile.isFile()}`)
-
+        const fileName= await fsPromises.readdir(basePath)
+        console.log(fileName)
+        for (const item of fileName) {
+            const stat= await fsPromises.stat(path.join(basePath, item))
+            console.log(`${item} is directory: ${stat.isDirectory()}`)
+        }
 
     }catch (e) {
         console.log(e)
